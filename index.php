bc581c0a50a77c4a4f0339c02a1c9542914c014b
plini <plini@plini-PC>
2017-10-27 22:25:30 -0200

    Meu terceiro commit

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        Essa é a primeira modificação.
     </body>
 </html>
